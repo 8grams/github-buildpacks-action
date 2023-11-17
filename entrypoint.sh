0e75d67205fa28ca9ea87a9cd6906fcd2875eda2
Glend Maatita <glend.maatita@gmail.com>
2023-11-17 08:58:36 +0700

    fix build push docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -l
 
-podman login -u "$INPUT_DOCKER_USERNAME" -p "$INPUT_DOCKER_PASSWORD"
 pack build "$INPUT_APP_NAME":"$INPUT_TAG_NAME" --builder gcr.io/buildpacks/builder:v1
-podman push "$INPUT_APP_NAME":"$INPUT_TAG_NAME"
-
