117831215381c6ff887b36631215a052cc300204
Glend Maatita <glend.maatita@gmail.com>
2023-11-17 08:42:34 +0700

    fix build push docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 
-RUN apk add curl
+RUN apk add curl podman
 
 RUN (curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.32.1/pack-v0.32.1-linux.tgz" | tar -C /usr/local/bin/ --no-same-owner -xzv pack)
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
 
-/usr/bin/docker login -u "$INPUT_DOCKER_USERNAME" -p "$INPUT_DOCKER_PASSWORD" && pack build "$INPUT_APP_NAME":"$INPUT_TAG_NAME" --builder gcr.io/buildpacks/builder:v1 --publish
+podman login -u "$INPUT_DOCKER_USERNAME" -p "$INPUT_DOCKER_PASSWORD"
+pack build "$INPUT_APP_NAME":"$INPUT_TAG_NAME" --builder gcr.io/buildpacks/builder:v1
+podman push "$INPUT_APP_NAME":"$INPUT_TAG_NAME"
+
