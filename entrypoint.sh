ea24ad50d7f9e9e78eae349fba0a53f88e1f4bfd
Glend Maatita <glend.maatita@gmail.com>
2023-11-17 08:37:16 +0700

    fix build push docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-docker login -u "$INPUT_DOCKER_USERNAME" -p "$INPUT_DOCKER_PASSWORD" && pack build "$INPUT_APP_NAME":"$INPUT_TAG_NAME" --builder gcr.io/buildpacks/builder:v1 --publish
+/usr/bin/docker login -u "$INPUT_DOCKER_USERNAME" -p "$INPUT_DOCKER_PASSWORD" && pack build "$INPUT_APP_NAME":"$INPUT_TAG_NAME" --builder gcr.io/buildpacks/builder:v1 --publish
